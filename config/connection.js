7911d2b233a64bbe326a85b23bd4cc18bd355152
jterry63 <jordankterry@outlook.com>
2018-07-30 22:11:23 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,16 @@ var mysql = require("mysql");
 var connection;
 
 
-var connection = mysql.createConnection({
-    use_env_variable: "JAWSDB_URL",
-    user: "rzyza88dd1zanzqr",
-    password: "je3mokf5ki6y8wfo",
-    port: "3306",
-    dialect: "mysql"
-    // host: "mnwax9mxzow0k7hi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+// var connection = mysql.createConnection({
+//     use_env_variable: "JAWSDB_URL",
+//     user: "rzyza88dd1zanzqr",
+//     password: "je3mokf5ki6y8wfo",
+//     port: "3306",
+//     dialect: "mysql"
+// host: "mnwax9mxzow0k7hi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
-
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// });
 
 
 connection.connect(function (err) {

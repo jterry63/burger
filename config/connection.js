3c6d6cdea31daa374544654aff30a46bc41e8248
jterry63 <jordankterry@outlook.com>
2018-07-30 21:53:24 -0600

    heroku-update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // connection information for MySQL 
 
 var mysql = require("mysql");
-var config_password = require("./config.js");
+
 
 var connection;
 
@@ -9,7 +9,7 @@ connection = mysql.createConnection({
     port: 3306,
     host: "us-cdbr-iron-east-04.cleardb.net",
     user: "b4031bfcf8037f:8f4c20dc",
-    password: config_password.pass,
+    password: "8f4c20dc",
     database: "heroku_90febd3c7bbf0b8"
 
 });
